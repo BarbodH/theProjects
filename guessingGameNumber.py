cb6485827b5988f039b1d341ad97eed1312bfb06
Barbod Hasanzadeh <barbod.hasanzadeh01@gmail.com>
2025-08-19 19:05:39 +0100

    guessingGameNumber.py

--- guessingGameNumber.py
+++ guessingGameNumber.py
@@ -1,23 +1,22 @@
 import random
 
-answer = random.randint(1 , 100)
-while True:
-    try: 
-        guess_count = int(input("How many times do you wanna play? "))
-        print("Enter a number between 1 and 100: ")
-        for _ in range(guess_count):
-            choice = int(input())
-            if answer > choice:
-                print("Enter a larger number: ")
-            elif answer < choice:
-                print("Enter a smaller number: ")
-            else:
-                print("")
-                print("YOU WON!!!")
-        if answer != choice:
-            print("")
-            print(f"Game Over! The correct answer is: {answer}")
-            break
+answer = random.randint(1, 100)
 
-    except ValueError:
-        print("Enter an integer!")
+guess_limit = int(input("How many times do you wanna play? "))
+
+print("Guess a number between 1 and 100: ")
+
+count = 0
+while count <= guess_limit:
+    choice = int(input())
+    count += 1
+    if choice == answer:
+        print("You Won!")
+        print(f"You guessed it in {count} {'try' if count == 1 else 'tries'}!")
+        break
+    elif answer > choice:
+        print("Enter a larger number: ")
+    else:
+        print("Enter a smaller number: ")
+else:
+    print(f"Game Over! The correct answer is {answer}.")
